fe4e27d682a2420afed1f38459c5b71acc1ad305
rirfanuddin <muhammad.rohman.irfanuddin09@gmail.com>
2017-11-20 09:46:00 +0700

    edit error === in web.php

--- routes/web.php
+++ routes/web.php
@@ -36,7 +36,7 @@ Route::prefix('admin')->group(function() {
 	Route::post('/login', 'Auth\AdminLoginController@login')->name('admin.login.submit');
 	Route::get('/', 'AdminController@index')->name('admin.dashboard');
 });
-=======
+
 Route::get('reserve', function () {
     return view('reserve');
 });
