ec54332b4398afd949430a3ad37969ccc3688073
Muhammad Ariq Srihadi Wibowoputra <muhammadariq13@gmail.com>
2017-11-20 10:49:42 +0700

    fix spacing

--- resources/views/reserve.blade.php
+++ resources/views/reserve.blade.php
@@ -50,102 +50,101 @@
 				</div>
 			</div>
 		</div>
-  <h3>Date only</h3>
-  <div class="ui calendar" id="example2">
-    <div class="ui input left icon">
-      <i class="calendar icon"></i>
-      <input type="text" placeholder="Date">
-    </div>
-  </div>
-  <div class="ui form">
-	<div class="two fields">
-	<div class="field">
+		<h3>Date only</h3>
+			<div class="ui calendar" id="example2">
+				<div class="ui input left icon">
+					<i class="calendar icon"></i><input type="text" placeholder="Date">
+				</div>
+			</div>
+			<div class="ui form">
+				<div class="two fields">
+					<div class="field">
 	
-  <label>Dewasa</label>
-		<select class="ui search dropdown">
-			<option value="">Jumlah</option>
-			<option value="1">1</option>
-			<option value="2">2</option>
-			<option value="3">3</option>
-			<option value="4">4</option>
-			<option value="5">5</option>
-			<option value="6">6</option>
-			<option value="7">7</option>
-			<option value="8">8</option>
-			<option value="9">9</option>
-			<option value="10">10</option>
-			<option value="11">11</option>
-			<option value="12">12</option>
-			<option value="13">13</option>
-			<option value="14">14</option>
-			<option value="15">15</option>
-			<option value="16">16</option>
-			<option value="17">17</option>
-			<option value="18">18</option>
-			<option value="19">19</option>
-			<option value="20">20</option>
-		</select>
-	</div>
-	<div class="field">
+					<label>Dewasa</label>
+					<select class="ui search dropdown">
+						<option value="">Jumlah</option>
+						<option value="1">1</option>
+						<option value="2">2</option>
+						<option value="3">3</option>
+						<option value="4">4</option>
+						<option value="5">5</option>
+						<option value="6">6</option>
+						<option value="7">7</option>
+						<option value="8">8</option>
+						<option value="9">9</option>
+						<option value="10">10</option>
+						<option value="11">11</option>
+						<option value="12">12</option>
+						<option value="13">13</option>
+						<option value="14">14</option>
+						<option value="15">15</option>
+						<option value="16">16</option>
+						<option value="17">17</option>
+						<option value="18">18</option>
+						<option value="19">19</option>
+						<option value="20">20</option>
+					</select>
+				</div>
+				<div class="field">
 	
-  <label>Anak-anak</label>
-		<select class="ui search dropdown">
-			<option value="">Jumlah</option>
-			<option value="0">0</option>
-			<option value="1">1</option>
-			<option value="2">2</option>
-			<option value="3">3</option>
-			<option value="4">4</option>
-			<option value="5">5</option>
-			<option value="6">6</option>
-			<option value="7">7</option>
-			<option value="8">8</option>
-			<option value="9">9</option>
-			<option value="10">10</option>
-			<option value="11">11</option>
-			<option value="12">12</option>
-			<option value="13">13</option>
-			<option value="14">14</option>
-			<option value="15">15</option>
-			<option value="16">16</option>
-			<option value="17">17</option>
-			<option value="18">18</option>
-			<option value="19">19</option>
-			<option value="20">20</option>
-		</select>
-	</div></br>
-	<div class="field">
+				<label>Anak-anak</label>
+				<select class="ui search dropdown">
+						<option value="">Jumlah</option>
+						<option value="0">0</option>
+						<option value="1">1</option>
+						<option value="2">2</option>
+						<option value="3">3</option>
+						<option value="4">4</option>
+						<option value="5">5</option>
+						<option value="6">6</option>
+						<option value="7">7</option>
+						<option value="8">8</option>
+						<option value="9">9</option>
+						<option value="10">10</option>
+						<option value="11">11</option>
+						<option value="12">12</option>
+						<option value="13">13</option>
+						<option value="14">14</option>
+						<option value="15">15</option>
+						<option value="16">16</option>
+						<option value="17">17</option>
+						<option value="18">18</option>
+						<option value="19">19</option>
+						<option value="20">20</option>
+					</select>
+				</div></br>
+				<div class="field">
 	
-  <label>Asal</label>
-		<select class="ui search dropdown">
-			<option value="">Jumlah</option>
-			<option value="1">1</option>
-			<option value="2">2</option>
-			<option value="3">3</option>
-			<option value="4">4</option>
-			<option value="5">5</option>
-			<option value="6">6</option>
-			<option value="7">7</option>
-			<option value="8">8</option>
-			<option value="9">9</option>
-			<option value="10">10</option>
-			<option value="11">11</option>
-			<option value="12">12</option>
-			<option value="13">13</option>
-			<option value="14">14</option>
-			<option value="15">15</option>
-			<option value="16">16</option>
-			<option value="17">17</option>
-			<option value="18">18</option>
-			<option value="19">19</option>
-			<option value="20">20</option>
-		</select>
-	</div>
-	<div class="field">
+				<label>Asal</label>
+				<select class="ui search dropdown">
+					<option value="">Asal Stasiun</option>
+					<option value="1">1</option>
+					<option value="2">2</option>
+					<option value="3">3</option>
+					<option value="4">4</option>
+					<option value="5">5</option>
+					<option value="6">6</option>
+					<option value="7">7</option>
+					<option value="8">8</option>
+					<option value="9">9</option>
+					<option value="10">10</option>
+					<option value="11">11</option>
+					<option value="12">12</option>
+					<option value="13">13</option>
+					<option value="14">14</option>
+					<option value="15">15</option>
+					<option value="16">16</option>
+					<option value="17">17</option>
+					<option value="18">18</option>
+					<option value="19">19</option>
+					<option value="20">20</option>
+				</select>
+			</div>
+			<div class="field">
 	
   <label>Tujuan</label>
 		<select class="ui search dropdown">
-			<option value="">Jumlah</option>
+			<option value="">Tujuan Stasiun</option>
 			<option value="0">0</option>
 			<option value="1">1</option>
 			<option value="2">2</option>
